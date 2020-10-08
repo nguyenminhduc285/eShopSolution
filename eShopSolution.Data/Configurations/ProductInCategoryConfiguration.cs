da697476ff6b0a3597d4d7631e5537a07a0bbb3a
ducsmith <duccoloa98@gmail.com>
2020-10-08 14:20:07 +0700

    finish seeing data

--- eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -15,9 +15,12 @@ namespace eShopSolution.Data.Configurations
 
             builder.ToTable("ProductInCategories");
 
-            builder.HasOne(t =>t.Product)
-                .WithMany(pc => pc.ProductInCategories)
-                .HasForeignKey(pc => pc.CategoryId);
+            builder.HasOne(t => t.Product).WithMany(pc => pc.ProductInCategories)
+                .HasForeignKey(pc => pc.ProductId);
+
+            builder.HasOne(t => t.Category).WithMany(pc => pc.ProductInCategories)
+              .HasForeignKey(pc => pc.CategoryId);
         }
     }
 }
+
---
+++ eShopSolution.Data/Migrations/20201008064707_SeedData.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInCategories_Categories_CategoryId1",
+                table: "ProductInCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInCategories_Products_CategoryId",
+                table: "ProductInCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductInCategories_CategoryId1",
+                table: "ProductInCategories");
+
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId1",
+                table: "ProductInCategories");
+
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "This is home page of eShopSolution" },
+                    { "HomeKeyword", "This is keyword eShopSolution" },
+                    { "HomeDescription", "This is description of eShopSolution" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi-VN", true, "Tieng Viet" },
+                    { "en-US", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "OriginalPrice", "Price" },
+                values: new object[] { 1, new DateTime(2020, 10, 8, 13, 47, 7, 315, DateTimeKind.Local).AddTicks(842), 100000m, 200000m });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTranslations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 2, "vi-VN", "Ao Khoac Nu", "Ao Cho Nu", "San pham ao thoi trang nu", "Ao Khoac" },
+                    { 3, 1, "vi-VN", "Ao Khoac Nam", "Ao Cho Nam", "San pham ao thoi trang nam", "Ao Khoac" },
+                    { 2, 2, "en-US", "Woman Jacket", "Jaket for Female", "The Fashion Product for Female", "Jacket" },
+                    { 4, 1, "en-US", "Men Jacket", "Jaket for Male", "The Fashion Product for Male", "Jacket" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Description", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, "", "Mo ta san pham", "vi-VN", "Ao Khoac Nam", 1, "Ao Cho Nam", "San pham ao thoi trang nam", "Ao Khoac" },
+                    { 2, "", "Description of product", "en-US", "Men Jacket", 1, "Jaket for Male", "The Fashion Product for Male", "Jacket" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInCategories_ProductId",
+                table: "ProductInCategories",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInCategories_Categories_CategoryId",
+                table: "ProductInCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInCategories_Products_ProductId",
+                table: "ProductInCategories",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInCategories_Categories_CategoryId",
+                table: "ProductInCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductInCategories_Products_ProductId",
+                table: "ProductInCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductInCategories_ProductId",
+                table: "ProductInCategories");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en-US");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi-VN");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId1",
+                table: "ProductInCategories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductInCategories_CategoryId1",
+                table: "ProductInCategories",
+                column: "CategoryId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInCategories_Categories_CategoryId1",
+                table: "ProductInCategories",
+                column: "CategoryId1",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductInCategories_Products_CategoryId",
+                table: "ProductInCategories",
+                column: "CategoryId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
