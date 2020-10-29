2bf4e55cc3ebd279bd56938752abc7dfdb15ff0c
ducsmith <duccoloa98@gmail.com>
2020-10-30 00:08:36 +0700

    api cho product

--- eShopSolution.BackendApi/Controllers/ProductController.cs
+++ eShopSolution.BackendApi/Controllers/ProductController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using eShopSolution.Application.Catalog.Products;
+﻿using eShopSolution.Application.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace eShopSolution.BackendApi.Controllers
 {
@@ -12,20 +10,77 @@ namespace eShopSolution.BackendApi.Controllers
     public class ProductController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
-        public ProductController(IPublicProductService publicProductService)
+        private readonly IManageProductService _manageProductService;
+        public ProductController(IPublicProductService publicProductService, IManageProductService manageProductService)
         {
             _publicProductService = publicProductService;
+            _manageProductService = manageProductService;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpGet("{languageId}")]
+        public async Task<IActionResult> Get(string languageId)
         {
-            var products = await _publicProductService.GetAll();
+            var products = await _publicProductService.GetAll(languageId);
+            return Ok(products);
+        }
+        //http://localhost:port/product/public-paging
+        [HttpGet("public-paging/{languageId}")]
+        public async Task<IActionResult> Get([FromQuery]GetPublicProductPagingRequest request)
+        {
+            var products = await _publicProductService.GetAllByCategoryId(request);
             return Ok(products);
         }
 
+        //http://localhost:port/product/1/vi-VN
+        [HttpGet("{id}/{languageId}")]
+        public async Task<IActionResult> GetById(int id, string languageId)
+        {
+            var product = await _manageProductService.GetById(id, languageId);
+            if (product == null)
+                return BadRequest("khong tim thay san pham");
+            return Ok(product);
+        }
 
-        
-       
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] ProductCreateRequest request)
+        {
+            var productId = await _manageProductService.Create(request);
+            if (productId == 0)
+                return BadRequest();
+
+            var product = await _manageProductService.GetById(productId, request.LanguageId);
+
+            return CreatedAtAction(nameof(GetById),new { id = productId} ,product);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] ProductUpdateRequest request)
+        {
+            var affectedResult = await _manageProductService.Update(request);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Ok();
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> Delete(int productId)
+        {
+            var affectedResult = await _manageProductService.Delete(productId);
+            if (affectedResult == 0)
+                return BadRequest();
+            return Ok();
+        }
+
+
+        [HttpPut("price/{id}/{newprice}")]
+        public async Task<IActionResult> UpdatePrice(int id,decimal newprice)
+        {
+            var isSuccessful = await _manageProductService.UpdatePrice(id,newprice);
+            if (!isSuccessful)
+                return BadRequest();
+            return Ok();
+        }
     }
 }
--- eShopSolution.BackendApi/Startup.cs
+++ eShopSolution.BackendApi/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using eShopSolution.Application.Catalog.Product;
 using eShopSolution.Application.Catalog.Products;
+using eShopSolution.Application.Common;
 using eShopSolution.Data.EF;
 using eShopSolution.Utilities.Constants;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,7 +29,9 @@ namespace eShopSolution.BackendApi
                 options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
 
             //Decalare DI
+            services.AddTransient<IStorageService, FileStorageService>();
             services.AddTransient<IPublicProductService, PublicProductService>();
+            services.AddTransient<IManageProductService, ManageProductService>();
 
             services.AddControllersWithViews();
 
