bcdd3cb5a30d32c37ad60fb48d07fd2297af72fe
ducsmith <duccoloa98@gmail.com>
2020-12-08 02:06:15 +0700

    drop downlist danh muc san pham chua ngon lam

--- eShopSolution.AdminApp/Startup.cs
+++ eShopSolution.AdminApp/Startup.cs
@@ -44,6 +44,7 @@ namespace eShopSolution.AdminApp
             services.AddTransient<ILanguageApiClient, LanguageApiClient>();
             services.AddTransient<ILanguageApiClient, LanguageApiClient>();
             services.AddTransient<IProductApiClient, ProductApiClient>();
+            services.AddTransient<ICategoryApiClient, CategoryApiClient>();
 
             IMvcBuilder builder = services.AddRazorPages();
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
