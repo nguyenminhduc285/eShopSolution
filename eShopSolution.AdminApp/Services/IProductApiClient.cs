6999a29605948ba971a629e36b0e04364abe89e1
ducsmith <duccoloa98@gmail.com>
2020-12-08 16:56:26 +0700

    tich category trong san pham

--- eShopSolution.AdminApp/Services/IProductApiClient.cs
+++ eShopSolution.AdminApp/Services/IProductApiClient.cs
@@ -1,5 +1,9 @@
 ﻿using eShopSolution.ViewModels.Catalog.Products;
 using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace eShopSolution.AdminApp.Services
@@ -9,5 +13,9 @@ namespace eShopSolution.AdminApp.Services
         Task<PagedResult<ProductVm>> GetPagings(GetManageProductPagingRequest request);
 
         Task<bool> CreateProduct(ProductCreateRequest request);
+
+        Task<ApiResult<bool>> CategoryAssign(int id, CategoryAssignRequest request);
+
+        Task<ProductVm> GetById(int id, string languageId);
     }
 }
