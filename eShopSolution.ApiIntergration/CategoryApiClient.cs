b951d333e280f4d0f134d40fa6e6738f63da2a47
ducsmith <duccoloa98@gmail.com>
2020-12-13 02:11:43 +0700

    load san pham theo danh muc

--- eShopSolution.ApiIntergration/CategoryApiClient.cs
+++ eShopSolution.ApiIntergration/CategoryApiClient.cs
@@ -1,8 +1,11 @@
 ﻿using eShopSolution.ApiIntegration;
 using eShopSolution.ViewModels.Catalog.Categories;
+using eShopSolution.ViewModels.Common;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -20,6 +23,11 @@ namespace eShopSolution.ApiIntegration
         public async Task<List<CategoryVm>> GetAll(string languageId)
         {
             return await GetListAsync<CategoryVm>("/api/categories?languageId=" + languageId);
+        }
+
+        public async Task<CategoryVm> GetById(string languageId, int id)
+        {
+            return await GetAsync<CategoryVm>($"/api/categories/{id}/{languageId}");
         }
     }
 }
